6652b8be536b94b3b832f91a4e4a615d26e05bf4
mritunjay singh <a34mritunjaysingh@gmail.com>
2024-12-21 12:39:10 +0530

    Fixed BUild error

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -51,36 +51,167 @@
 
 // const handler = NextAuth(authOptions);
 
+// // export { handler as GET, handler as POST };
+// import NextAuth from 'next-auth';
+// import GithubProvider from 'next-auth/providers/github';
+// import connectDB from '@/lib/db';
+// import User from '@/models/User';
+
+// export const authOptions = {
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//       scope: 'user:email read:user repo'
+//     }),
+//   ],
+//   callbacks: {
+//     async signIn({ user, account, profile }) {
+//       if (account.provider === 'github') {
+//         await connectDB();
+        
+//         console.log('GitHub Profile:', profile);
+        
+//         const existingUser = await User.findOne({ githubId: user.id });
+//         if (existingUser) {
+//           existingUser.accessToken = account.access_token;
+//           existingUser.githubUsername = profile.login; // GitHub username
+//           await existingUser.save();
+//         } else {
+//           await User.create({
+//             githubId: user.id,
+//             githubUsername: profile.login, // Use profile.login here
+//             accessToken: account.access_token,
+//           });
+//         }
+//       }
+//       return true;
+//     },
+//     async jwt({ token, account, profile }) {
+//       if (account && profile) {
+//         token.accessToken = account.access_token;
+//         token.githubUsername = profile.login; // Add to JWT token
+//       }
+//       return token;
+//     },
+//     async session({ session, token }) {
+//       console.log('Token in session callback:', token);
+      
+//       await connectDB();
+//       const dbUser = await User.findOne({ githubId: token.sub });
+      
+//       if (dbUser) {
+//         session.user.accessToken = dbUser.accessToken;
+//         session.user.id = dbUser._id;
+//         session.user.githubId = dbUser.githubId;
+//         session.user.githubUsername = dbUser.githubUsername;
+//       }
+      
+//       console.log('Final session:', session);
+      
+//       return session;
+//     },
+//   },
+//   debug: true,
+// };
+
+// const handler = NextAuth(authOptions);
 // export { handler as GET, handler as POST };
+
+
+
+// import NextAuth, { NextAuthOptions } from 'next-auth';
+// import GithubProvider from 'next-auth/providers/github';
+// import connectDB from '@/lib/db';
+// import User from '@/models/User';
+
+// export const authOptions: NextAuthOptions = { // Explicit typing here
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID || '', // Add fallback to avoid undefined values
+//       clientSecret: process.env.GITHUB_SECRET || '',
+//       scope: 'user:email read:user repo'
+//     }),
+//   ],
+//   callbacks: {
+//     async signIn({ user, account, profile }) {
+//       if (account.provider === 'github') {
+//         await connectDB();
+        
+//         console.log('GitHub Profile:', profile);
+        
+//         const existingUser = await User.findOne({ githubId: user.id });
+//         if (existingUser) {
+//           existingUser.accessToken = account.access_token;
+//           existingUser.githubUsername = profile.login; // GitHub username
+//           await existingUser.save();
+//         } else {
+//           await User.create({
+//             githubId: user.id,
+//             githubUsername: profile.login, // Use profile.login here
+//             accessToken: account.access_token,
+//           });
+//         }
+//       }
+//       return true;
+//     },
+//     async jwt({ token, account, profile }) {
+//       if (account && profile) {
+//         token.accessToken = account.access_token;
+//         token.githubUsername = profile.login; // Add to JWT token
+//       }
+//       return token;
+//     },
+//     async session({ session, token }) {
+//       console.log('Token in session callback:', token);
+      
+//       await connectDB();
+//       const dbUser = await User.findOne({ githubId: token.sub });
+      
+//       if (dbUser) {
+//         session.user.accessToken = dbUser.accessToken;
+//         session.user.id = dbUser._id;
+//         session.user.githubId = dbUser.githubId;
+//         session.user.githubUsername = dbUser.githubUsername;
+//       }
+      
+//       console.log('Final session:', session);
+      
+//       return session;
+//     },
+//   },
+//   debug: true,
+// };
+
+
+// @ts-nocheck
+// @ts-nocheck
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import connectDB from '@/lib/db';
 import User from '@/models/User';
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      scope: 'user:email read:user repo'
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+      scope: 'user:email read:user repo',
     }),
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
-      if (account.provider === 'github') {
+      if (account?.provider === 'github') {
         await connectDB();
-        
-        console.log('GitHub Profile:', profile);
-        
         const existingUser = await User.findOne({ githubId: user.id });
         if (existingUser) {
           existingUser.accessToken = account.access_token;
-          existingUser.githubUsername = profile.login; // GitHub username
+          existingUser.githubUsername = profile?.login;
           await existingUser.save();
         } else {
           await User.create({
             githubId: user.id,
-            githubUsername: profile.login, // Use profile.login here
+            githubUsername: profile?.login,
             accessToken: account.access_token,
           });
         }
@@ -90,30 +221,25 @@ export const authOptions = {
     async jwt({ token, account, profile }) {
       if (account && profile) {
         token.accessToken = account.access_token;
-        token.githubUsername = profile.login; // Add to JWT token
+        token.githubUsername = profile.login;
       }
       return token;
     },
     async session({ session, token }) {
-      console.log('Token in session callback:', token);
-      
       await connectDB();
       const dbUser = await User.findOne({ githubId: token.sub });
-      
-      if (dbUser) {
+      if (dbUser && session.user) {
         session.user.accessToken = dbUser.accessToken;
-        session.user.id = dbUser._id;
+        session.user.id = dbUser._id.toString();
         session.user.githubId = dbUser.githubId;
         session.user.githubUsername = dbUser.githubUsername;
       }
-      
-      console.log('Final session:', session);
-      
       return session;
     },
   },
   debug: true,
 };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+// Explicitly export the HTTP method handlers as individual functions
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
